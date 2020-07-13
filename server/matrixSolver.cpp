1c2d2d05ae3012433979db11e2aa9191bbf1c684
Danio <daniowang20101@gmail.com>
2020-07-13 16:14:02 -0700

    create two folders one for client and one for server

--- server/matrixSolver.cpp
+++ server/matrixSolver.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <boost/asio.hpp>
-using namespace boost::asio;
+#include <boost/system/error_code.hpp>
 using namespace boost::asio;
 using ip::tcp;
 using std::string;
@@ -15,7 +15,7 @@ void send_(tcp::socket & socket, const string& message) {
 
 string read_(tcp::socket & socket) {
     boost::asio::streambuf buf;
-    boost::asio::read_until(socket, buf, "\n");
+    boost::asio::read_until(socket, buf, "\n ｜ ");
     string data = boost::asio::buffer_cast<const char*>(buf.data());
     return data;
 }
@@ -24,16 +24,18 @@ string read_(tcp::socket & socket) {
 int main() {
     std::cout<<"This is a matrix solver."<<std::endl;
     boost::asio::io_service io_service;
-    tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 80));
-    tcp::socket socket_(io_service);
-    acceptor_.accept(socket_);
+    tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v6(), 80)); //acceptor represents acceptor socket which is pasisve
     while (1) {
         try {
+        tcp::socket socket_(io_service);
+        acceptor_.accept(socket_);
         string msg = read_(socket_);
         cout << "server side message: " << msg << endl;
         //write operation
         send_(socket_, "hello this is a matrix solver");
         cout << "request has been sent" << endl;
+        } catch(boost::system::system_error & sysError) {
+            cout << "System Error | Error Code: " <<sysError.code() << " | Error Message: " << sysError.what()<<endl;
         } catch (...) {
             cout << "recieved an invalid request" << endl;
         }
